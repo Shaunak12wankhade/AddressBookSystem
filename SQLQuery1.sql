3e31d75c12dacf95ee0e76815f10b4e4ff6f8c24
unknown <shaunak12wankhade@ggmail.com>
2021-11-12 09:15:33 +0530

    [Shaunak] Added : Addressbook table with attributes

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -5,3 +5,18 @@
 
 CREATE DATABASE AddressBookService;
 USE AddressBookService;
+
+--UC2--
+---Create Addressbook table---
+
+CREATE TABLE AddressBookTable(
+FirstName varchar(100) not null,
+LastName varchar(50),
+Address varchar(200),
+City varchar(150),
+StateName varchar(100),
+ZipCode int,
+Phonenum bigint,
+EmailId varchar(100)
+);
+select * from AddressBookTable;
